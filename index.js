88c23b3eab5b158b95c7db67c347d51bee02f0c5
elvis-visi <sivisevi1@gmail.com>
2024-01-27 19:24:07 +0100

    Using database in route handlers

--- index.js
+++ index.js
@@ -21,27 +21,6 @@ app.use(express.json());
 app.use(requestLogger);
 app.use(express.static("dist"));
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     date: "2022-01-10T17:30:31.098Z",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only Javascript",
-//     date: "2022-01-10T18:39:34.091Z",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2022-01-10T19:20:14.298Z",
-//     important: true,
-//   },
-// ];
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -56,22 +35,18 @@ const generateId = () => {
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.get("/api/notes", (request, response) => {
@@ -88,14 +63,9 @@ app.delete("/api/notes/:id", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.put("/api/notes/:id", (request, response) => {
