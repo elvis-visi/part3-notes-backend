5b557cc4f7a2466017ed1f935730879ff1e7c2ac
elvis-visi <sivisevi1@gmail.com>
2024-01-27 17:41:38 +0100

    mongoDB example

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.xjms7ej.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+//Models are responsible for creating and reading documents from the
+//underlying MongoDB database.
+const Note = mongoose.model("Note", noteSchema);
+
+//document -> an instance of a model
+const note = new Note({
+  content: "CSS is hard",
+  important: true,
+});
+
+// note.save().then((result) => {
+//   console.log("result,", result);
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
