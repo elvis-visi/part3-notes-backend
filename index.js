2bdba6f2c1239cffc3bd74b24c5a0334ff3f7f2b
elvis-visi <sivisevi1@gmail.com>
2024-01-27 19:38:08 +0100

    Testing backend

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const Note = require("./models/note");
-app.use(cors());
+require("dotenv").config();
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -17,6 +16,7 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(express.static("dist"));
