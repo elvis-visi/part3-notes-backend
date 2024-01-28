b9e30d054b61b7e1cdebcdc211a4238804ea719d
elvis-visi <sivisevi1@gmail.com>
2024-01-28 10:08:14 +0100

    Error handling

--- index.js
+++ index.js
@@ -16,12 +16,20 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
 app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(requestLogger);
-app.use(express.static("dist"));
-
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
@@ -62,10 +70,16 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (request, response) => {
@@ -81,8 +95,10 @@ app.put("/api/notes/:id", (request, response) => {
     response.status(404).end();
   }
 });
-
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint);
+// handler of requests with result to errors
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
