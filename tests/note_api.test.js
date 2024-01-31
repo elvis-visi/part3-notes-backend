542768a8b465df176aa6872a35e0fbba0f0c0b6d
elvis-visi <sivisevi1@gmail.com>
2024-01-31 11:16:01 +0100

    beforeEach, ensuring DB state is the same before tests are run

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -5,6 +5,28 @@ const app = require('../app')
 //wrap the app in a superagent object
 //tests can use it to make HTTP requests to the backend
 const api = supertest(app)
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+//ensuring DB is in the same state before every test is run
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
 
 test('notes are returned as json', async () => {
   await api
@@ -13,16 +35,20 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 },100000)
 
-test('there are two notes', async () => {
+test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  expect(response.body).toHaveLength(1)
+  expect(response.body).toHaveLength(initialNotes.length)
 })
 
-test('the first note is about HTTP methods', async () => {
+test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
 
-  expect(response.body[0].content).toBe('HTML is easy')
+  const contents = response.body.map(r => r.content)
+  expect(contents).toContain(
+    'Browser can execute only JavaScript'
+  )
+
 })
 
 //afterAll - runs a function after all tests in this file have completed
