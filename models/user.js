cfa7c2e65fdb84cc95af49deec0831b84a650817
elvis-visi <sivisevi1@gmail.com>
2024-02-01 18:41:30 +0100

    Added user model, both user and note store references to each other

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  //each object represents a reference to a document in the Note collection
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note'
+    }
+  ],
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
+
+
+
+/*
+When you use Mongoose to query a User document, you can optionally populate the notes field.
+
+instead of just getting the IDs of the notes associated with the user, Mongoose will fetch
+the full Note documents from their collection and include them in the query result. This is
+done through the .populate() method provided by Mongoose.
+
+
+*/
